a9837d2b983aa60225f1c3aa949414392dedaec6
Gio <44237862+Redifexx@users.noreply.github.com>
2024-01-30 20:22:49 -0500

    Lots

--- src/simpleTexture.cpp
+++ src/simpleTexture.cpp
@@ -1,3 +1,4 @@
+#define GLEW_STATIC
 #include <GL/glew.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
@@ -156,8 +157,8 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
     // Create the image (RGB Array) to be displayed
-    const int width = 8; // keep it in powers of 2!
-    const int height = 8; // keep it in powers of 2!
+    const int width = 16; // keep it in powers of 2!
+    const int height = 16; // keep it in powers of 2!
     unsigned char image[width * height * 3];
     for (int i = 0; i < height; i++)
     {
@@ -165,7 +166,7 @@ int main()
         {
             int idx = (i * width + j) * 3;
             image[idx] = (unsigned char)(255 * i * j / height / width); //((i+j) % 2) * 255;
-            image[idx + 1] = 0;
+            image[idx + 1] = (unsigned char)(255 * i * j / height / width);
             image[idx + 2] = 0;
         }
     }
